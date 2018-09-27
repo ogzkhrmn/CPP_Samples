c6494cf72dfd15c138a1aa4af0c00b353965330f
Oğuz Kahraman <ogz.khrmn@hotmail.com>
2018-09-27 13:30:28 +0300

    Addes multiplechoice.

--- examples/Source1.cpp
+++ examples/Source1.cpp
@@ -4,15 +4,24 @@
 
 int main()
 {
-	
-	//Time t;
 
-	//t.setTime(12, 10, 0);
-	//t.printMilitary();
+	int userChoice;
+
+	cout << "1: Time Example\n2:Sales Example\nInsert your choice: " << endl;
+	cin >> userChoice;
+	Time t;
 	salesp sp;
-
-	sp.getSalesFromUser();
-	sp.printAnnualSales();
+	switch (userChoice)
+	{
+	case 1:
+		t.setTime(12, 10, 0);
+		t.printMilitary();
+	case 2:
+		sp.getSalesFromUser();
+		sp.printAnnualSales();
+	default:
+		cout << "Wrong button. " << endl;
+	}
 
 	return 0;
 }
